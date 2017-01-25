4c4f44d6f709c9d490762996bcb977ff66552eb8
Aurelie <aurelie.debrot@he-arc.ch>
2017-01-25 14:45:47 +0100

    first table

--- linkyou/admin.py
+++ linkyou/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django import forms
+from .models import Link
 
 # Register your models here.
+class LinkAdminForm(forms.ModelForm):
+    class Meta:
+        model = Link
+        fields = "__all__"
+
+class LinkAdmin(admin.ModelAdmin):
+    form = LinkAdminForm
+
+admin.site.register(Link, LinkAdmin)
--- linkyou/models.py
+++ linkyou/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+    url= models.URLField(max_length=2000)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return u'%s' % self.name
