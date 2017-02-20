ec52c1daf0ebe50f249c2b8afa66bbf419eaaa28
Dom <ducommun.dom@gmail.com>
2017-02-20 13:37:01 +0100

    Created templates folder, base layout file and Home page view

--- linkyou/urls.py
+++ linkyou/urls.py
@@ -1,8 +1,9 @@
 
 from django.conf.urls import url
 
+
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^', views.Home.as_view(), name='index'),
 ]
--- linkyou/views.py
+++ linkyou/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class Home(TemplateView):
+    '''LinkYou homepage with concept description and call to action'''
+    def get(self, request):
+        return render(request, "home.html")
