11793a0665119900e8ca14dd10bc98e64053ff35
Dom <ducommun.dom@gmail.com>
2017-02-20 14:46:43 +0100

    Base empty views + base templates
    Views for pages, ready for collections using generic views, adapted routes and templates for Home, About and Discover pages

--- linkyou/urls.py
+++ linkyou/urls.py
@@ -1,9 +1,8 @@
-
 from django.conf.urls import url
-
-
 from . import views
 
 urlpatterns = [
-    url(r'^', views.Home.as_view(), name='index'),
+    url(r'^about', views.About.as_view(), name='about'),            # The about page
+    url(r'^discover', views.Discover.as_view(), name='discover'),   # The discover collections page
+    url(r'^', views.Home.as_view(), name='home'),                   # The homepage
 ]
