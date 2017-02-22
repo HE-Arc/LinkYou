121807039ab51e063bfddf48854bdbcb409dd4d6
axelroy <royaxe@gmail.com>
2017-02-22 14:50:00 +0100

    Models description for Collection with foreign keys and migrations update

--- linkyouapp/models.py
+++ linkyouapp/models.py
@@ -1,9 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Collection(models.Model):
+    name= models.CharField(max_length=255)
+    icon = models.URLField(max_length=2000)
+    slug = models.SlugField(unique=True, max_length = 200)
+    user_it_belongs = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+    )
+
+
+    def __str__(self):
+        return u'%s' % self.name
+
 class Link(models.Model):
     url= models.URLField(max_length=2000)
-    name = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    collection_it_belongs = models.ForeignKey(
+        'Collection',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+    )
 
     def __str__(self):
         return u'%s' % self.name
