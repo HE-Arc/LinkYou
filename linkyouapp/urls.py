d556b9926316984a52bd4fd97301e3160f6a3d03
LumenH <aurelie.debrot@gmail.com>
2017-02-22 14:28:42 +0100

    connection page

--- linkyouapp/urls.py
+++ linkyouapp/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^accounts/',include('registration.backends.simple.urls'), name='registration_register'),
     url(r'^login', auth_views.login, name='login'),
-    url(r'^logout', auth_views.logout, name='logout'), 
+    url(r'^logout', auth_views.logout, name='logout'),
     url(r'^about', views.About.as_view(), name='about'),            # The about page
     url(r'^discover', views.Discover.as_view(), name='discover'),   # The discover collections page
     url(r'^', views.Home.as_view(), name='home'),                   # The homepage
