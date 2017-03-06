1ba4f19161358100dac9ebde005148b13debdbeb
Dom <ducommun.dom@gmail.com>
2017-03-06 12:06:50 +0100

    Generic listviews for home and dashboard

--- linkyouapp/views.py
+++ linkyouapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, DeleteView, ListView
 from .models import Collection, Link
+from django.contrib.auth.models import User
 from .forms import CollectionForm, LinkForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -11,10 +12,18 @@ from django.http import HttpResponse
 
 
 # Static pages related views
-class Home(TemplateView):
+class Home(ListView):
     '''LinkYou homepage with concept description and call to action'''
-    def get(self, request):
-        return render(request, "home.html")
+    context_object_name = 'featured_collection_list'
+    queryset = Collection.objects.order_by("-pk")[:3] #  TODO: filter featured = True ou most liked ?
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)      # Le contexte de base defini au dessus
+        context['last_collection_list'] = Collection.objects.order_by("-pk")[:4] # Un ajout pour avoir les dernieres collections
+        context['last_users_list'] = User.objects.order_by("-pk")[:5]  # Un ajout pour avoir les derniers utilisateurs
+        return context
+
 
 class About(TemplateView):
     '''You know, if we have the time to do it'''
@@ -30,11 +39,17 @@ class Discover(TemplateView):
 
 
 # User related views
-class UserDashboardView(LoginRequiredMixin, TemplateView):
+class UserDashboardView(LoginRequiredMixin, ListView):
     '''The dashboard of a user and his collections list'''
-    # Coming when models exist
-    pass
+    context_object_name = 'user_collections_list'
+    template_name = 'dashboard.html'
 
+    def get_queryset(self):
+        return Collection.objects.filter(user_it_belongs=self.request.user)
+
+class UserProfileView(TemplateView):
+    def get(self, request):
+        return render(request, "profile.html")
 
 # Collection related views
 class CollectionDetailView(DetailView):
