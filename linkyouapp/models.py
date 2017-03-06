e75665a345b097f2fca64bf857085a76128f29be
axelroy <royaxe@gmail.com>
2017-03-06 13:44:01 +0100

    Add the tags to the Collection

--- linkyouapp/models.py
+++ linkyouapp/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from tagging.fields import TagField
 
 class Collection(models.Model):
     name= models.CharField(max_length=255)
     slug = models.SlugField(unique=True, max_length = 200)
     image = models.ImageField()
     likes = models.IntegerField()
+    tags = TagField()
     user_it_belongs = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
