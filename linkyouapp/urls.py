39fa6f6de24a24425e9d758d4f22f0d464ef6a83
Dom <ducommun.dom@gmail.com>
2017-03-08 20:01:59 +0100

    Basic features : add link in collection, display collection

--- linkyouapp/urls.py
+++ linkyouapp/urls.py
@@ -5,7 +5,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^accounts/',include('registration.backends.simple.urls'), name='registration_register'),
     url(r'^collection/new', views.CollectionCreateView.as_view(), name='new_collection'),
-    url(r'^link/new', views.LinkCreateView.as_view(), name='new_link'),
+    url(r'^collection/(?P<slug>[\w-]+)-(?P<pk>\w+)', views.CollectionDetailView.as_view(), name='collection_detail'),
+    url(r'^(?P<pk>\w+)-(?P<slug>[\w-]+)/link/new', views.LinkCreateView.as_view(), name='new_link'),
     url(r'^login', auth_views.login, name='login'),
     url(r'^logout', auth_views.logout, name='logout'),
     url(r'^dashboard', views.UserDashboardView.as_view(), name='dashboard'),
