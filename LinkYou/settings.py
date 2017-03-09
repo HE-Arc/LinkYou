2b200d67ef0d618621e84645a23c0ff8b0146144
Dom <ducommun.dom@gmail.com>
2017-03-09 09:59:34 +0100

    Django-tagging -> django-taggit

--- LinkYou/settings.py
+++ LinkYou/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TAGGIT_CASE_INSENSITIVE = True
 
 # Application definition
 
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tagging'
+    'taggit',
 ]
 
 MIDDLEWARE = [
--- LinkYou/urls.py
+++ LinkYou/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+# For the medias in debug mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
     url(r'^', include('linkyouapp.urls')),
 ]
