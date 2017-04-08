04850bef8603f72414fff134d5aa32d0ce01c672
Dom <ducommun.dom@gmail.com>
2017-04-08 15:04:58 +0200

    Minor adjustments, form input classes

--- linkyouapp/forms.py
+++ linkyouapp/forms.py
@@ -5,8 +5,16 @@ class CollectionForm(forms.ModelForm):
     class Meta:
         model = Collection
         fields = {'name', 'image', 'tags', 'private'}
+        widgets = {
+                    'name': forms.TextInput(attrs={'placeholder': 'My great collection'}),
+
+                }
 
 class LinkForm(forms.ModelForm):
     class Meta:
         model = Link
         fields = {'url', 'text'}
+        widgets = {
+                    'text': forms.TextInput(attrs={'placeholder': 'My new link'}),
+                    'url': forms.TextInput(attrs={'placeholder': 'http://www.example.com'}),
+                }
